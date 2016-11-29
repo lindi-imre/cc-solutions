a5a98a781cf7d28f5af8ff648ae671da64a11a1c
lindi-imre <lindi.imre@gmail.com>
2016-11-29 16:35:06 +0100

    add archive function

--- To-Do app/main.py
+++ To-Do app/main.py
@@ -2,7 +2,7 @@
 
 #üres lista, ebbe fogjuk menteni a a todo elemeinket
 todo_list = []
-#üres list, ide mentjük, hogy valami meg van-e jelölve
+#üres lista, ide mentjük, hogy valami meg van-e jelölve
 mark_list = []
 # Ezt a két listát egy dictionary-vel is lehetne helyettesíteni
 
@@ -45,6 +45,29 @@ def mark_todo():
     todo_number -= 1
     mark_list[todo_number] = True
 
+
+# Ez a függvény kitörli mindkét listából a megadott indexen lévő elemeket, érdemes átnézni, mivel a törlés nem egyszerű
+# dolog python, mert ha törlünk akkor a következő elemek egy indexel előrébb csúsznak pl:
+# 0 1 2 3 4 5 6 7 -> 3. elem törlése 0 1 2 3 4 5 6 mostmár látszik, hogy a 4ik elem volt az u, most pedig a 3ik lett 
+# f e q w u t w q                    f e q u t w q
+def archive_todos():
+    global todo_list
+    global mark_list
+
+    iterator = 0
+    while iterator < len(todo_list):
+        # ha mark list[iterator] True akkor mindkát listából törlöm az elemet. ha pl:
+        # iterator egyenlő 3al és mark_list[iterator] True akkor mark_list és todo_list-ből
+        # is törli a 3ik elemet
+        if mark_list[iterator]:
+            del mark_list[iterator]
+            del todo_list[iterator]
+        else:
+            #azért csak else ágban iterálok, mert ha törlök és pl törlöm a 3ik elemet akkor a 4ik elem a 3ik helyére csúszik
+            # így legközelebb is a 3ik elemet kell megnéznem
+            iterator += 1
+    print("All completed tasks got deleted.")
+
 # itt csak kiprinteljük a menüt és bekérjük mit szeretne a user
 def print_menu():
     command = input("Please specify a command [list, add, mark, archive]: ")
@@ -54,7 +77,8 @@ def print_menu():
         add_todo()
     elif command == "mark":
         mark_todo()
-
+    elif command == "archive":
+        archive_todos()
 
 while True:
     print_menu()
